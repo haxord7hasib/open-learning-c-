b2d98715eceba449925996d95b816ca1c3a39bb0
haxord7-Hasib <haxord7@gmail.com>
2020-02-01 08:48:14 +0600

    Create vowel or consonant.c
    This code has a limitation find it!

---
+++ vowel or consonant.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+
+    char a;
+    char e;
+    char i;
+    char o;
+    char u;
+    char word;
+
+    scanf("%c", &word);
+    
+    if (word== 'a') {
+        printf("%c vowel" ,word);
+    }
+    
+    else if (word== 'e') {
+        printf("%c vowel" ,word);
+    }
+     else if (word== 'i') {
+        printf("%c vowel" ,word);
+    }
+    else if (word== 'o') {
+        printf("%c vowel" ,word);
+    }
+     else if (word== 'u') {
+        printf("%c vowel" ,word);
+    }
+    else {
+    	printf("Consonant"); 
+	}
+    return 0;
+}
