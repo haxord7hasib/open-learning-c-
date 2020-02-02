73b38ce8848ca7752d06695d7b18cf0dc702d778
haxord7-Hasib <haxord7@gmail.com>
2020-02-02 20:33:13 +0600

    Create powerfunction.c
    This code has power function .

---
+++ powerfunction.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+
+
+int x;
+int y;
+int z;
+int k;
+ x = 5;
+ y =90 ;
+
+z= x+y;
+
+k= pow(z,2);
+printf ("Resultis \n %d",k);
+
+
+return (0);
+
+
+
+}
